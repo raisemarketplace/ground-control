6898530b29eaaee944e7795f465b23e97f99b262
Nick Dreckshage <nicholas.dreckshage@raise.com>
2016-01-11 10:28:45 -0600

    removing some text

--- easy-example/routes.js
+++ easy-example/routes.js
@@ -6,23 +6,8 @@ import { simpleConnect } from 'modules/AsyncNestedRedux';
 import _AppComponent from 'easy-example/components';
 import HomeComponent, { reducer as homeReducer } from 'easy-example/components/home';
 import Route2Component, { reducer as route2Reducer, fetchData as route2FetchData } from 'easy-example/components/route-2';
-
-// connect top level only & everything is automatic.
 const AppComponent = simpleConnect(connect, _AppComponent);
 
-// adjusted route api...
-// {
-//   path,
-//   component,
-//   childRoutes,
-//   getChildRoutes,
-//   ...
-//   reducer (optional),
-//   fetchData (optional),
-//   loader (optional),
-// }
-
-// routes can be defined using jsx, or as an object (my preference, especially with larger app)
 export default (
   <Route path="/" component={AppComponent}>
     <IndexRoute component={HomeComponent} reducer={homeReducer} />
